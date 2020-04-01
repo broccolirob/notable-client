22cab34ebace87b90237974d921d91e706390eef
broccolirob <rschn4911@gmail.com>
2020-04-01 10:44:41 -0400

    Commit to jwt auth strategy. Remove firebase and auth0 related packages and services.

--- src/app/auth/store/actions/login.actions.js
+++ src/app/auth/store/actions/login.actions.js
@@ -1,6 +1,4 @@
-import firebaseService from 'app/services/firebaseService';
 import jwtService from 'app/services/jwtService';
-import * as Actions from 'app/store/actions';
 import * as UserActions from './user.actions';
 
 export const LOGIN_ERROR = 'LOGIN_ERROR';
@@ -21,48 +19,6 @@ export function submitLogin({ email, password }) {
 				return dispatch({
 					type: LOGIN_ERROR,
 					payload: error
-				});
-			});
-}
-
-export function submitLoginWithFireBase({ username, password }) {
-	if (!firebaseService.auth) {
-		console.warn("Firebase Service didn't initialize, check your configuration");
-
-		return () => false;
-	}
-
-	return dispatch =>
-		firebaseService.auth
-			.signInWithEmailAndPassword(username, password)
-			.then(() => {
-				return dispatch({
-					type: LOGIN_SUCCESS
-				});
-			})
-			.catch(error => {
-				console.info('error');
-				const usernameErrorCodes = [
-					'auth/email-already-in-use',
-					'auth/invalid-email',
-					'auth/operation-not-allowed',
-					'auth/user-not-found',
-					'auth/user-disabled'
-				];
-				const passwordErrorCodes = ['auth/weak-password', 'auth/wrong-password'];
-
-				const response = {
-					username: usernameErrorCodes.includes(error.code) ? error.message : null,
-					password: passwordErrorCodes.includes(error.code) ? error.message : null
-				};
-
-				if (error.code === 'auth/invalid-api-key') {
-					dispatch(Actions.showMessage({ message: error.message }));
-				}
-
-				return dispatch({
-					type: LOGIN_ERROR,
-					payload: response
 				});
 			});
 }
