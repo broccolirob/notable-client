5300604564fbaa9088c7c652d3fde321f011ed18
broccolirob <rschn4911@gmail.com>
2020-04-04 16:57:38 -0400

    Pull tagging out of bash script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker image build -t $DOCKER_HUB_REPO ../
 docker image push $DOCKER_HUB_REPO
 docker logout
